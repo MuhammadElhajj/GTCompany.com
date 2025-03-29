a2190e0a130567bcefc097724a11e6125f1316fb
MuhammadElhajj <3muhammad.el.hajj@gmail.com>
2025-03-30 01:26:12 +0300

    sexth commit

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -35,17 +35,38 @@ function Home() {
       <div className="Home__Container">
         <form id="emailForm" onSubmit={onSubmit} method="POST">
           <h1 className="Home__Container__Recipients">
-            Recipients Information
+            1 _ Recipients Information
           </h1>
-          <textarea
-            name="first_name_list"
-            id="first_name_list"
-            placeholder="Recipient names each on a new line."
-            rows="10"
-            required=""
-            oninput="validateRowCounts()"
-          ></textarea>
-          <h3 className="Home__Container__From__To">From & Reply-To</h3>
+          <div className="Home__Container__Names__Email__Recipients">
+            <div className="Recipients__Name">
+              <h3 className="Recipients__Name__h3">Names</h3>
+              <textarea
+                name="first_name_list"
+                id="first_name_list"
+                placeholder="Recipient names each on a new line. Ex :
+1 _ Julia 
+2 _ Masa 
+          "
+                rows="10"
+                required=""
+                oninput="validateRowCounts()"
+              ></textarea>
+            </div>
+            <div className="Recipients__Email">
+            <h3 className="Recipients__Email__h3">Email</h3>
+              <textarea
+                name="email_list"
+                id="email_list"
+                placeholder="Recipient emails each on a new line. Ex :
+1 _ Julia@gmail.com
+2 _ Masa@gmail.com  "
+                rows="10"
+                required
+                oninput="validateRowCounts()"
+              ></textarea>
+            </div>
+          </div>
+          <h3 className="Home__Container__From__To">2 _ From & Reply-To</h3>
           <div className="Home__Container__From__To__Container">
             <div className="Home__Container__From__To__Container__From">
               <h3 className="Home__Container__From__Name">From</h3>
@@ -78,14 +99,26 @@ function Home() {
               />
             </div>
           </div>
-          <h3 className="Home__Container__From__To">Title & Email</h3>
+          <h3 className="Home__Container__From__To">3 _ Title & Email</h3>
           <div className="Home__Container__Title__Email">
-            <input
+            <div className="Home__Container__Title__Email__Email">
+            <h3 className="Home__Container__To__Name">Email Title </h3>
+            {/* <input
               type="text"
               name="email_title"
               placeholder="Email Title"
               required
-            />
+            /> */}
+            <textarea
+              type="text"
+              name="email_title"
+              placeholder="Email Title"
+              required
+            >
+          </textarea>
+            </div>
+            <div className="Home__Container__Title__Email__Message">
+            <h3 className="Home__Container__To__Name">Message </h3>
             <textarea
               name="email_message"
               placeholder="Email Message (HTML allowed). Use {first_name} to personalize the message."
@@ -93,8 +126,9 @@ function Home() {
               required
             ></textarea>
           </div>
+          </div>
           <div className="Home__Container__Chosse__File">
-            <h3 className="Home__Container__From__To">Send File</h3>
+            <h3 className="Home__Container__From__To">4 _ Send File</h3>
             <input type="file" name="attachments[]" id="attachments" multiple />
           </div>
 
